28e54a1f110b3de82ba9feeed87139a92a03d108
geraldfernandez-coder <gerald_fernandez@hotmail.fr>
2026-03-13 13:28:46 +0100

    Add files via upload

--- app.py
+++ app.py
@@ -29,7 +29,7 @@ BG_DIR = APP_DIR / "bg"
 st.set_page_config(page_title="FilmFinder IA", layout="centered")
 
 # ================== THEME ==================
-VARIANT = "B"
+VARIANT = "C"
 THEMES = {
     "A": {
         "name": "Bulles Messenger",
